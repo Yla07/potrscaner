12fd60bb0c42805c910b34b8d31e9f5311e977fb
Bastian <bastianwic07@gmail.com>
2025-12-19 09:02:54 +0100

    Refactor port scanning function and improve connection check

--- portscan.py
+++ portscan.py
@@ -1,5 +1,5 @@
 import nmap as nm
-
+import os
 # target = input("Target IP:")
 #pstart = int(input("Initital port: "))
 #pend = int(input("Final port: "))
@@ -12,16 +12,37 @@ import nmap as nm
 #    print(f'Port {i} {state}')
 
 
-def scan(t, port):
+def port(t, port):
     scan = nm.PortScanner()
-    out = scan.scan(t, str(port))
+    out = scan.port(t, str(port))
     state = out['scan'][t]['tcp'][port]['state']
     protocol = out['scan'][t]['tcp'][port].get('name', 'unknown')
-    print("Port ", port, "is ", state, ". Protocol ", protocol)
-
+    if state == "open":
+     print("Port ", port, "is open", ". Protocol ", protocol)
+    else:
+       return False
 t = input ("Target:")
 ps = input("Starting port: ")
 pe = input("End port: ")
+x = 0
 
-for i in range (int(ps), int(pe) + 1):
-    scan(t,i)
+print ("Checking connection ...")
+
+res = os.system(f"ping {t} > /dev/null 2>&1" if os.name != "nt" else f"ping {t} >nul 2>&1")
+
+if res == 0:
+    print (f"{t} is up")
+    for i in range (int(ps), int(pe) + 1):
+        if port(t,i) == False:
+            x+=1
+        else:
+            continue
+
+    print ("There are ", x, "closed ports")
+else:
+   print("Host is down.🖕")
+
+
+
+
+
